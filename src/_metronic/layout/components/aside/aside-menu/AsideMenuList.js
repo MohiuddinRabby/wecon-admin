94afd0f7388ae666b363b6f51f356527c346a83a
golam@@0177 <rabbymohiuddin@gmail.com>
2022-04-02 16:57:31 +0600

    init update

---
+++ src/_metronic/layout/components/aside/aside-menu/AsideMenuList.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-script-url,jsx-a11y/anchor-is-valid */
+import React from "react";
+import { useLocation } from "react-router";
+import { NavLink } from "react-router-dom";
+import SVG from "react-inlinesvg";
+import { toAbsoluteUrl, checkIsActive } from "../../../../_helpers";
+
+export function AsideMenuList({ layoutProps }) {
+  const location = useLocation();
+  const getMenuItemActive = (url, hasSubmenu = false) => {
+    return checkIsActive(location, url)
+      ? ` ${!hasSubmenu && "menu-item-active"} menu-item-open `
+      : "";
+  };
+
+  return (
+    <>
+      {/* begin::Menu Nav */}
+      <ul className={`menu-nav ${layoutProps.ulClasses}`}>
+        {/*begin::1 Level*/}
+        <li
+          className={`menu-item ${getMenuItemActive("/dashboard", false)}`}
+          aria-haspopup="true"
+        >
+          <NavLink className="menu-link" to="/dashboard">
+            <span className="svg-icon menu-icon">
+              <SVG src={toAbsoluteUrl("/media/svg/icons/Design/Layers.svg")} />
+            </span>
+            <span className="menu-text">Dashboard</span>
+          </NavLink>
+        </li>
+        {/*end::1 Level*/}
+        {/* Applications */}
+        {/* begin::section */}
+        <li className="menu-section ">
+          <h4 className="menu-text">Applications</h4>
+          <i className="menu-icon flaticon-more-v2"></i>
+        </li>
+        {/* end:: section */}
+      </ul>
+
+      {/* end::Menu Nav */}
+    </>
+  );
+}
